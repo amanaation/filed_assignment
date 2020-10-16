20bde1d58133f43b7a121ab772ab06382a91b889
Aman Mishra <amanmishramax345@gmail.com>
2020-10-16 18:31:56 +0530

    Modified main_functions.py

--- main_functions.py
+++ main_functions.py
@@ -3,24 +3,28 @@ from datetime import datetime
 
 def process_transaction(sent_data):
     try:
-        amount = sent_data['amount']
         if  valid_transaction(sent_data):
-            try:
-                amount = float(amount)
-            except:
-                return "The request is invalid: 400 bad request"
-            if amount > 0:
-                if amount <= 20:
-                    return CheapPaymentGateway(sent_data)
-                elif amount > 20 and amount <= 500:
-                    return ExpensivePaymentGateway(sent_data)
-                elif amount > 500:
-                    return PremiumPaymentGateway(sent_data)
+            amount = sent_data['amount']
+            return valid_amount(amount)
         else:
             return "The request is invalid: 400 bad request"
     except Exception as e:
-        print(e)
-        return str(e)
+        return "Any error: 500 internal server error"
+
+def valid_amount(amount):
+    try:
+        amount = float(amount)
+    except:
+        return "The request is invalid: 400 bad request"
+    if amount > 0:
+        if amount <= 20:
+            return CheapPaymentGateway(amount)
+        elif amount > 20 and amount <= 500:
+            return ExpensivePaymentGateway(amount)
+        elif amount > 500:
+            return PremiumPaymentGateway(amount)
+    else:
+        return "The request is invalid: 400 bad request"
 
 def valid_credit_card_number(number):
     return len(number) == 16 and type(number) is str
@@ -43,28 +47,20 @@ def valid_expiration_date(date):
     else:
         return False
 
-
 def valid_security_code(code):
     if code != "":
         return code.isnumeric() and len(code) == 3
     return True
 
 def valid_transaction(sent_data):
-    print(sent_data)
     if valid_credit_card_number(sent_data["ccnumber"]):
-        print("Passed ccnumber")
         if valid_card_holder(sent_data["holder"]):
-            print("Passed holder")
             if valid_expiration_date(sent_data["expirationdate"]):
-                print("Passed expirationdate")
                 if valid_security_code(sent_data["securitycode"]):
-                    print("Passed securitycode")
                     return True
     return False
 
 def PremiumPaymentGateway(data_to_be_processed):
-    print("PremiumPaymentGateway")
-
     for i in range(3):
         print("Attempt Number : ", i)
         if True:
@@ -72,13 +68,11 @@ def PremiumPaymentGateway(data_to_be_processed):
     return False
 
 def ExpensivePaymentGateway(data_to_be_processed):
-    print("ExpensivePaymentGateway")
     if True:
         return True
     return CheapPaymentGateway()
 
 def CheapPaymentGateway(data_to_be_processed):
-    print("CheapPaymentGateway")
     if True:
         return True
     return False
